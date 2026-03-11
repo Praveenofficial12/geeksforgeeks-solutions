15e5ee71dfbe0948bea775351f9310e12fd3645b
Praveenofficial12 <216186201+Praveenofficial12@users.noreply.github.com>
2026-03-11 20:21:25 +0530

    Concatenation of Array to Concatenation of Array

--- Concatenation of Array.java
+++ Concatenation of Array.java
@@ -1,8 +1,11 @@
-class Solution:
-    def getConcatenation(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        ans = [0] * (2 * n)
-        for i in range(n):
-            ans[i] = nums[i]
-            ans[i + n] = nums[i]
-        return ans
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2 * n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+        return ans;
+    }
+}
